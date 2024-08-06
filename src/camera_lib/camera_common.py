e0189e527134c6229f5f11c2b90d60d73e6050ae
Z3ZEL <66113155+Z3ZEL@users.noreply.github.com>
2024-08-06 14:33:34 +1200

    [Desactivate Nice]

--- src/camera_lib/camera_common.py
+++ src/camera_lib/camera_common.py
@@ -1,4 +1,4 @@
-import subprocess, psutil
+import subprocess
 from camera_lib.camera_var import CONVERT_CMD, CAMERA_LOG, SHOT_CMD
 
 def convert():
@@ -10,7 +10,7 @@ def convert():
 
 def launch_shot():
     photo = subprocess.Popen(SHOT_CMD(0).split(" "),stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    p = psutil.Process(photo.pid)
-    p.nice(-20)
+    # p = psutil.Process(photo.pid)
+    # p.nice(-20)
 
     return photo
