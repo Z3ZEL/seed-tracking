683ec14707a9410e45bbcdcf1f4d42bb95c27090
Z3ZEL <66113155+Z3ZEL@users.noreply.github.com>
2024-08-20 14:24:22 +1200

    [HOTFIX]

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Create a Python virtual environment named 'venv'
-python3 -m venv .env
+python3 -m venv --system-site-packages .env
 # Install dependencies from requirements.txt
 ./.env/bin/pip install -r requirements.txt
 echo "Environment setup and dependencies installed."
